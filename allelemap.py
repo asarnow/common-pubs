56a3d76e1890aec18990f8a4f97a3d3e109f390c
Daniel Asarnow <dasarnow@gmail.com>
2015-11-03 16:42:24 -0800

    Convert to str from Seq.

--- allelemap.py
+++ allelemap.py
@@ -10,12 +10,12 @@ def main(args):
     library = pik.load(open(args.dbfile, 'rb'))
     allele = {}
     for k in library:
-        s = Seq.Seq(k, Seq.Alphabet.SingleLetterAlphabet()).reverse_complement()
+        s = str(Seq.Seq(k, Seq.Alphabet.SingleLetterAlphabet()).reverse_complement())
         pos = int(library[k][0])
         if pos != 0:
             cod = Seq.Seq(library[k][1], Seq.Alphabet.SingleLetterAlphabet())
             aa = cod.transcribe().translate()
-            allele[s] = [pos, cod, aa, 0]
+            allele[s] = [pos, str(cod), aa, 0]
         else:
             allele[s] = [pos, None, None, 0]
 
