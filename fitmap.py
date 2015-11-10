21f27e2c22fbd0c6b81b320dd46f5e8fbe2ce626
Daniel Asarnow <dasarnow@gmail.com>
2015-11-09 22:58:11 -0800

    Fitmap ought to work with:  * Single multiplexed FASTQ  * Multiple demultipexled FASTQs representing one series  * Previously cached fitmap output Needs testing when DeRisi cluster back online.

--- fitmap.py
+++ fitmap.py
@@ -19,64 +19,62 @@ from pandas.io import pytables
 
 
 def main(args):
-    if args.out is None:
-        args.out = "."
-    if len(args.files) > 0 and ".fastq" in args.files[0]:
-        if len(args.files) > 1:
+    if len(args.files) > 0:  # Data files passed on command line.
+        if len(args.files) > 1:  # Demultiplexed data files.
             meta, ac, oc = process_fastq_files(args)
-            expdefs = { None: [(file2col(f),) for f in args.files]}
-        else:
+            expdefs = { None: [(file2col(f),) for f in args.files]}  # Use file names.
+        else:  # Single multiplexed data file.
             if args.expdefs is None:
                 print "Experiment definitions required."
                 return 1
             with open(args.expdefs, 'rb') as f:
                 expdefs = pik.load(f)
             meta, ac, oc = process_fastq_files(args, [v[0] for v in [expdefs[k] for k in expdefs]])
-        if args.cache is not None:
+        if args.cache is not None:  # Use cache to store parsed data.
             store = pytables.HDFStore(args.out)
             store['metadata'] = meta
             store['allele_counts'] = ac
             store['other_counts'] = oc
             store.close()
-    elif args.cache is not None:
-        if args.expdefs is None:
-            print "Experiment definitions required."
-            return 1
-        with open(args.expdefs, 'rb') as f:
-            expdefs = pik.load(f)
+    elif args.cache is not None:  # Load data from cache.
         cache = pytables.HDFStore(args.cache)
         ac = cache['allele_counts']
         oc = cache['other_counts']
         meta = cache['metadata']
         cache.close()
+        if args.expdefs is None:
+            print "Experiment definitions required."
+            return 1
+        with open(args.expdefs, 'rb') as f:
+            expdefs = pik.load(f)
     else:
         print "Specify at least one data source."
         return 1
 
-    wt = ac[meta['Pos'] == 0]
-    vswt = np.log2(ac / np.sum(wt))
-
-    for k in expdefs:
-        cols = [t[0] for t in expdefs[k]]
-        pool = Pool(processes=args.numproc)
-        result = pool.map(linregress_wrapper, vswt[cols].values)
-        fitness = pd.DataFrame(data=result, index=vswt.index, columns=['slope', 'intercept', 'r', 'p', 'stderr'])
-        pool.close()
+    data = convert_data(ac, meta)
 
     if args.cache is not None:
         cache = pytables.HDFStore(args.cache)
-        cache['fitness'] = fitness
+        if 'fitness' in cache and not args.recompute:  # Load cached fitness data.
+            fitness = cache['fitness']
+        else:  # Recompute and cache fitness data.
+            fitness = compute_fitness(args, data, expdefs)
+            cache['fitness'] = fitness
         cache.close()
+    else:  # Compute fitness data on-the-fly.
+        fitness = compute_fitness(args, data, expdefs)
 
-    draw_hist(fitness, os.path.join(args.out, 'fitmap_hist.png'))
-    idx = meta.reset_index().set_index(['Pos', 'AA', 'Codon'])
-    aa = list(set(meta['AA']) - {None, np.nan})
-    cod = list(set(meta['Codon']) - {None, np.nan})
-    pos = list(set(meta['Pos']) - {None, np.nan, 0})
-    aamaps = compute_hmap(fitness['slope'], pos, aa, 'Pos', 'AA', idx, [np.nanmedian, np.nanstd])
-    codmaps = compute_hmap(fitness['slope'], pos, cod, 'Pos', 'Codon', idx, [np.nanmedian, np.nanstd])
-    draw_hmap(aamaps[0], aa, os.path.join(args.out, 'fitmap_aa.png'))
-    draw_hmap(codmaps[0], cod, os.path.join(args.out, 'fitmap_cod.png'))
+    if args.out is not None:  # Draw figures in output directory.
+        draw_hist(fitness, os.path.join(args.out, 'fitmap_hist.png'))
+        idx = meta.reset_index().set_index(['Pos', 'AA', 'Codon'])
+        aa = list(set(meta['AA']) - {None, np.nan})
+        cod = list(set(meta['Codon']) - {None, np.nan})
+        pos = list(set(meta['Pos']) - {None, np.nan, 0})
+        aamaps = compute_hmap(fitness['slope'], pos, aa, 'Pos', 'AA', idx, [np.nanmedian, np.nanstd])
+        codmaps = compute_hmap(fitness['slope'], pos, cod, 'Pos', 'Codon', idx, [np.nanmedian, np.nanstd])
+        draw_hmap(aamaps[0], aa, os.path.join(args.out, 'fitmap_aa_median.png'))
+        draw_hmap(codmaps[0], cod, os.path.join(args.out, 'fitmap_cod_median.png'))
+        
     return 0
 
 
@@ -123,6 +121,23 @@ def linregress_wrapper(y):
     return slope, intercept, rval, pval, err
 
 
+def convert_data(ac, meta):
+    wt = ac[meta['Pos'] == 0]
+    vswt = np.log2(ac / np.sum(wt))
+    return vswt
+
+
+def compute_fitness(args, data, expdefs):
+    fitness = {k: [] for k in expdefs}
+    for k in expdefs:
+        cols = [t[0] for t in expdefs[k]]
+        pool = Pool(processes=args.numproc)
+        result = pool.map(linregress_wrapper, data[cols].values)
+        fitness[k] = pd.DataFrame(data=result, index=data.index, columns=['slope', 'intercept', 'r', 'p', 'stderr'])
+        pool.close()
+    return fitness
+
+
 def parse_library(dbfile):
     library = pik.load(open(dbfile, 'rb'))
     allele = {}
@@ -167,12 +182,13 @@ def parse_fastq_file(fastq, counts, others):
         f = open(fastq, 'rU')
     records = SeqIO.parse(f, 'fastq', Seq.Alphabet.DNAAlphabet())
     for r in records:
+        index = r.description[-6:]
         putative = str(r.seq[0:18])
-        if putative in counts:
-            counts[putative] += 1
+        if putative in counts[index]:
+            counts[index][putative] += 1
             # r.letter_annotations['phred_quality']
         else:
-            others[putative] += 1
+            others[index][putative] += 1
             # r.letter_annotations['phred_quality']
     f.close()
 
@@ -225,6 +241,8 @@ if __name__ == "__main__":
                         dest="expdefs", help="Experiment definitions.")
     parser.add_argument("-c", "--cache", action="store", dest="cache", default=None,
                         help="Cache file path.")
+    parser.add_argument("--force-recompute", action="store_true", dest="recompute",
+                        default=False, help="Force recalculation of fitness scores.")
     parser.add_argument("-o", "--outdir", action="store", type=str,
                         dest="out", help="Output figure directory.")
     parser.add_argument("files", nargs='*')
